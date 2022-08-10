0faf1589f503e67d6d77f044aa9f574665cfb45c
muhnuramin <muhnuramin956@gmail.com>
2022-08-10 18:17:57 +0700

    fix view member admin

--- application/views/admin/moduls/V_member.php
+++ application/views/admin/moduls/V_member.php
@@ -48,47 +48,47 @@ defined('BASEPATH') or die('No direct script access allowed!');
 
                                 <div class="col-md-12">
 
-								<label>Username</label>
-									<input type="text" class="form-control" name="username_member" required>
-									<label>Password</label>
-									<input type="text" class="form-control" name="password_member" required>
-									<label>Nama</label>
-									<input type="text" class="form-control" name="nama_member" required>
-									<label>Email</label>
-									<input type="text" class="form-control" name="email_member" required>
-									<label>NO HP</label>
-									<input type="text" class="form-control" name="no_hp_member" required>
-									<label>Website</label>
-									<input type="text" class="form-control" name="website_member" required>
-									<fieldset class="form-group">
-                        <div class="col-md-12">
-						
-                            <div class="col-md-12">
-							<label>  Status</label>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" id="status_member" name="status_member" value="1" >
-                                    <label class="form-check-label" for="status_member">
-                                        Member
-                                    </label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" id="status_member2" name="status_member" value="0" >
-                                    <label class="form-check-label" for="status_member2">
-                                        Bukan Member
-                                    </label>
-                                </div>
-                                <small class="text-danger">
-                                    <?php echo form_error('status_member') ?>
-                                </small>
-                            </div>
-                        </div>
-                    </fieldset>
-						
+                                    <label>Username</label>
+                                    <input type="text" class="form-control" name="username_member" required>
+                                    <label>Password</label>
+                                    <input type="text" class="form-control" name="password_member" required>
+                                    <label>Nama</label>
+                                    <input type="text" class="form-control" name="nama_member" required>
+                                    <label>Email</label>
+                                    <input type="text" class="form-control" name="email_member" required>
+                                    <label>NO HP</label>
+                                    <input type="text" class="form-control" name="no_hp_member" required>
+                                    <label>Website</label>
+                                    <input type="text" class="form-control" name="website_member" required>
+                                    <fieldset class="form-group">
+                                        <div class="col-md-12">
+
+                                            <div class="col-md-12">
+                                                <label> Status</label>
+                                                <div class="form-check form-check-inline">
+                                                    <input class="form-check-input" type="radio" id="status_member" name="status_member" value="1">
+                                                    <label class="form-check-label" for="status_member">
+                                                        Member
+                                                    </label>
+                                                </div>
+                                                <div class="form-check form-check-inline">
+                                                    <input class="form-check-input" type="radio" id="status_member2" name="status_member" value="0">
+                                                    <label class="form-check-label" for="status_member2">
+                                                        Bukan Member
+                                                    </label>
+                                                </div>
+                                                <small class="text-danger">
+                                                    <?php echo form_error('status_member') ?>
+                                                </small>
+                                            </div>
+                                        </div>
+                                    </fieldset>
+
                                 </div>
 
                             </div>
 
-                          
+
 
                         </div>
 
@@ -114,59 +114,59 @@ defined('BASEPATH') or die('No direct script access allowed!');
 
             <thead>
 
-			<th>No</th>
+                <th>No</th>
 
-<th>Id</th>
+                <th>Id</th>
 
-<th>Username</th>
+                <th>Username</th>
 
-<th>Password</th>
-<th>Nama</th>
-<th>Email</th>
-<th>No HP</th>
-<th>Website</th>
-<th>Status</th>
+                <th>Password</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>No HP</th>
+                <th>Website</th>
+                <th>Status</th>
                 <th>Action</th>
 
             </thead>
 
             <tbody>
 
-			<?php $i = 1; ?>
+                <?php $i = 1; ?>
 
-<?php foreach ($member as $k) : ?>
+                <?php foreach ($member as $k) : ?>
 
-	<tr>
+                    <tr>
 
-		<td><?= $i++; ?></td>
+                        <td><?= $i++; ?></td>
 
-		<td><?= $k['id_member'] ?></td>
-		<td><?= $k['username_member'] ?></td>
-		<td><?= $k['password_member'] ?></td>
-		<td><?= $k['nama_member'] ?></td>
-		<td><?= $k['email_member'] ?></td>
-		<td><?= $k['no_hp_member'] ?></td>
-		<td><?= $k['website_member'] ?></td>
-		<!-- <td><?= $k['status_member'] ?><br><br> -->
-<!-- 		
+                        <td><?= $k['id_member'] ?></td>
+                        <td><?= $k['username_member'] ?></td>
+                        <td><?= $k['password_member'] ?></td>
+                        <td><?= $k['nama_member'] ?></td>
+                        <td><?= $k['email_member'] ?></td>
+                        <td><?= $k['no_hp_member'] ?></td>
+                        <td><?= $k['website_member'] ?></td>
+                        <!-- <td><?= $k['status_member'] ?><br><br> -->
+                        <!-- 		
 		<a href="<?= base_url('member_c/updatemember/'); ?><?= $k['id_member']; ?>"  type="button" class="btn  btn-xs" data-toggle="tooltip" data-placement="auto" value="0"title="Ubah Status" onclick="return confirm('Yakin ingin mengubah Status?');"><i class="far "> Ubah</i></a>
 
 			
-	<?php if($k->status_member === '1'): ?>
+	<?php if ($k['status_member'] === '1') : ?>
 		<a href="<?= base_url('member_c/updatemember/'); ?><?= $k['id_member']; ?>"  type="button" class="btn  btn-xs" data-toggle="tooltip" data-placement="auto" value="0"title="Ubah Status" onclick="return confirm('Yakin ingin mengubah Status?');"><i class="far "> Ubah</i></a>
-		<?php else: ?>
+		<?php else : ?>
 			<a href="<?= base_url('member_c/updatemember/'); ?><?= $k['id_member']; ?>"  type="button" class="btn  btn-xs" data-toggle="tooltip" data-placement="auto" value="0"title="Ubah Status" onclick="return confirm('Yakin ingin mengubah Status?');"><i class="far "> Ubah</i></a>
 		<?php endif ?>  -->
-				
-		<?php if($k->status_member === '1'): ?>
-			<td class="text-center text-gray">Member</td>
-		<?php else: ?>
-			<td class="text-center text-green">Bukan member</td>
-		<?php endif ?> 
+
+                        <?php if ($k['status_member'] === '1') : ?>
+                            <td class="text-center text-gray">Member</td>
+                        <?php else : ?>
+                            <td class="text-center text-green">Bukan member</td>
+                        <?php endif ?>
 
                         <td>
 
-                            <a href="#editbuyers" type="button" class="btn btn-primary btn-xs" data-toggle="modal" id="btn-edit-buyers_materi" data-id_buyers="<?= $k['id_member']; ?>" data-nama_buyers="<?= $k['username_member'] ?>" ><i class="fa fa-edit"> Edit</i></a>
+                            <a href="#editbuyers" type="button" class="btn btn-primary btn-xs" data-toggle="modal" id="btn-edit-buyers_materi" data-id_buyers="<?= $k['id_member']; ?>" data-nama_buyers="<?= $k['username_member'] ?>"><i class="fa fa-edit"> Edit</i></a>
 
                             <a href="<?= base_url('member_c/delete/'); ?><?= $k['id_member']; ?>" type="button" class="btn btn-danger btn-xs" data-toggle="tooltip" data-placement="auto" title="Hapus Surat" onclick="return confirm('Yakin ingin menghapus data');"><i class="far fa-trash-alt"> Hapus</i></a>
 
@@ -217,41 +217,41 @@ defined('BASEPATH') or die('No direct script access allowed!');
 
                             <input type="text" class="form-control" name="status_buyers_materi" id="edit_status_buyers_materi" required> -->
 
-							<label>Username</label>
-									<input type="text" class="form-control" name="username_member" required>
-									<label>Password</label>
-									<input type="text" class="form-control" name="password_member" required>
-									<label>Nama</label>
-									<input type="text" class="form-control" name="nama_member" required>
-									<label>Email</label>
-									<input type="text" class="form-control" name="email_member" required>
-									<label>NO HP</label>
-									<input type="text" class="form-control" name="no_hp_member" required>
-									<label>Website</label>
-									<input type="text" class="form-control" name="website_member" required>
-									<fieldset class="form-group">
-                        <div class="col-md-12">
-						
-                            <div class="col-md-12">
-							<label>  Status</label>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" id="status_member" name="status_member" value="1" >
-                                    <label class="form-check-label" for="status_member">
-                                        Member
-                                    </label>
+                            <label>Username</label>
+                            <input type="text" class="form-control" name="username_member" required>
+                            <label>Password</label>
+                            <input type="text" class="form-control" name="password_member" required>
+                            <label>Nama</label>
+                            <input type="text" class="form-control" name="nama_member" required>
+                            <label>Email</label>
+                            <input type="text" class="form-control" name="email_member" required>
+                            <label>NO HP</label>
+                            <input type="text" class="form-control" name="no_hp_member" required>
+                            <label>Website</label>
+                            <input type="text" class="form-control" name="website_member" required>
+                            <fieldset class="form-group">
+                                <div class="col-md-12">
+
+                                    <div class="col-md-12">
+                                        <label> Status</label>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" id="status_member" name="status_member" value="1">
+                                            <label class="form-check-label" for="status_member">
+                                                Member
+                                            </label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" id="status_member2" name="status_member" value="0">
+                                            <label class="form-check-label" for="status_member2">
+                                                Bukan Member
+                                            </label>
+                                        </div>
+                                        <small class="text-danger">
+                                            <?php echo form_error('status_member') ?>
+                                        </small>
+                                    </div>
                                 </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" id="status_member2" name="status_member" value="0" >
-                                    <label class="form-check-label" for="status_member2">
-                                        Bukan Member
-                                    </label>
-                                </div>
-                                <small class="text-danger">
-                                    <?php echo form_error('status_member') ?>
-                                </small>
-                            </div>
-                        </div>
-                    </fieldset>
+                            </fieldset>
 
                         </div>
 
@@ -288,7 +288,6 @@ defined('BASEPATH') or die('No direct script access allowed!');
 </div>
 
 <script>
-
     $(document).on('click', '#btn-edit-data_buyers', function() {
 
         let id_buyers = $(this).data('id_buyers');
@@ -306,5 +305,4 @@ defined('BASEPATH') or die('No direct script access allowed!');
         // $('#edit_nama_buyers_en').val(nama_buyers_en);
 
     })
-
 </script>
